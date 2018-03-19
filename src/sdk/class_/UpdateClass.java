f2eff7def4caa1503e6b822d0c2133ec511dfcce
yangjing <yangjingami@qq.com>
2018-03-19 18:09:26 +0800

    命名

--- src/sdk/class_/UpdateClass.java
+++ src/sdk/class_/UpdateClass.java
@@ -1,7 +1,7 @@
 package sdk.class_;
 
 import sdk.Const;
-import sdk.organization.AddTeacher;
+import sdk.core.AddTeacher;
 import utils.FileUtil;
 import utils.HttpJSONRequest;
 
@@ -9,7 +9,7 @@ public class UpdateClass {
 	private static String path = UpdateClass.class.getResource("").getPath();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String json = FileUtil.readStream(path+"updateClass.json");
+		String json = FileUtil.readStream(path+"UpdateClass.json");
 		String result = HttpJSONRequest.post(Const.DO_MAIN_URL+"third/api/class/update/nologin", json);
 		System.out.println(result);
 	}
