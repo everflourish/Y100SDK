d0f5b3efe2097f46d592f5f27cb6ca97d3d9013c
xieqiuming <xieqiuming5529@dingtalk.com>
2018-03-28 11:10:32 +0800

    考试列表接口

--- src/sdk/Const.java
+++ src/sdk/Const.java
@@ -1,7 +1,7 @@
 package sdk;
 
 public class Const {
-	public static final String KEY = "xqm";//账号
+	public static final String KEY = "test";//账号
 	public static final String SECRET = "123456";//秘钥
 	public static final String DO_MAIN_URL = "http://localhost:8089/yeah100-svc-web/";//接口地址ַ
 	public static final String LOGIN_URL = "http://localhost:8089/yeah100-web-new/login_by_name.html";//登录地址
